7d1bc9b2a115ad0c576171a7bb33d04efe25547d
abxjdorn <abxjdorn@users.noreply.github.com>
2020-02-26 11:51:28 -0700

    initial StreamPage

--- social_frontend/components/app/App.js
+++ social_frontend/components/app/App.js
@@ -6,31 +6,37 @@ import {
   NavLink,
 } from 'react-router-dom';
 
+import StreamPage from './StreamPage';
 
 // TODO remove the component definition here, move to own folders
-const test = () => (<div>Hello World from test</div>);
 const test1 = () => (<div>Hello World from test1</div>);
 const test2 = () => (<div>Hello World from test2</div>);
 const test3 = () => (<div>Hello World from test3</div>);
 
+const Header = () => (
+  <header>
+    <NavLink to="/" exact activeClassName="header-btn-active" className="header-btn">
+      Home
+    </NavLink>
+    <NavLink to="/test1" activeClassName="header-btn-active" className="header-btn">
+      Test1
+    </NavLink>
+    <NavLink to="/test2" activeClassName="header-btn-active" className="header-btn">
+      Test2
+    </NavLink>
+    <NavLink to="/test3" exact activeClassName="header-btn-active" className="header-btn">
+      Test3
+    </NavLink>
+  </header>
+);
+
 const App = () => (
   <Router>
-    <div className="header-container">
-      <NavLink to="/" exact activeClassName="header-btn-active" className="header-btn">
-        Home
-      </NavLink>
-      <NavLink to="/test1" activeClassName="header-btn-active" className="header-btn">
-        Test1
-      </NavLink>
-      <NavLink to="/test2" activeClassName="header-btn-active" className="header-btn">
-        Test2
-      </NavLink>
-      <NavLink to="/test3" exact activeClassName="header-btn-active" className="header-btn">
-        Test3
-      </NavLink>
-    </div>
+    <Header />
     <Switch>
-      <Route path="/" exact component={test} />
+      <Route path="/" exact>
+        <StreamPage />
+      </Route>
       <Route path="/test1" component={test1} />
       <Route path="/test2" component={test2} />
       <Route path="/test3" component={test3} />
---
+++ social_frontend/components/app/StreamPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => <div className="profile">Profile</div>;
+
+const Stream = () => <div className="stream">Stream</div>;
+
+const StreamPage = () => (
+  <main>
+    <Profile />
+    <Stream />
+  </main>
+);
+
+export default StreamPage;
