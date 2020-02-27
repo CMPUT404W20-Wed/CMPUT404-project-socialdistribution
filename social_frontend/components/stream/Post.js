4a3c5372cdcdcc210219f42793b6e6bd7eb72290
abxjdorn <abxjdorn@users.noreply.github.com>
2020-02-26 20:05:11 -0700

    post footers + some css

--- social_frontend/components/stream/Post.js
+++ social_frontend/components/stream/Post.js
@@ -13,20 +13,40 @@ PostHeader.propTypes = {
 };
 
 
+const PostFooter = ({ commentCount }) => {
+  if (commentCount) {
+    return (
+      <footer className="post-footer">
+        {commentCount.toLocaleString()}
+        {commentCount > 1 ? ' comments' : ' comment'}
+      </footer>
+    );
+  }
+
+  return null;
+};
+
+PostFooter.propTypes = {
+  commentCount: PropTypes.number.isRequired,
+};
+
+
 // TODO Pasting the content directly into the HTML is not the
 // correct way to handle actual posts from the server!
-const Post = ({ author, content }) => (
+const Post = ({ author, content, commentCount }) => (
   <article className="post">
     <PostHeader author={author} />
     <div className="content-text">
       {content}
     </div>
+    <PostFooter commentCount={commentCount} />
   </article>
 );
 
 Post.propTypes = {
   author: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  commentCount: PropTypes.number.isRequired,
 };
 
 export default Post;
--- social_frontend/components/stream/Stream.js
+++ social_frontend/components/stream/Stream.js
@@ -9,11 +9,54 @@ import './stream.css';
 export default class Stream extends React.Component {
   state = {
     posts: [
-      { id: '0', author: 'Author1ID', content: 'Post 1' },
-      { id: '1', author: 'Author2ID', content: 'Post 2' },
-      { id: '2', author: 'Author3ID', content: 'Post 3' },
-      { id: '3', author: 'Author4ID', content: 'Post 4' },
-      { id: '4', author: 'Author5ID', content: 'Post 5' },
+      {
+        id: '0',
+        author: 'Author1ID',
+        content: 'In the end, it didn\'t work, but we discovered several things along the way: first, that whatever you do, you can\'t do anything you thought you maybe couldn\'t do unless you at least tell yourself you can try; second, that whatever you wanted to do, it may or may not already have been done; and third, that writing example text is actually kind of difficult.',
+        commentCount: 0,
+      },
+      {
+        id: '1',
+        author: 'Author2ID',
+        content: 'So what I think is going on here is actually nothing in particular.',
+        commentCount: 13,
+      },
+      {
+        id: '2',
+        author: 'Author3ID',
+        content: 'Post 3',
+        commentCount: 1,
+      },
+      {
+        id: '3',
+        author: 'Author4ID',
+        content: 'Post 4',
+        commentCount: 0,
+      },
+      {
+        id: '4',
+        author: 'Author5ID',
+        content: 'Post 5',
+        commentCount: 0,
+      },
+      {
+        id: '5',
+        author: 'Author1ID',
+        content: 'In the end, it didn\'t work, but we discovered several things along the way: first, that whatever you do, you can\'t do anything you thought you maybe couldn\'t do unless you at least tell yourself you can try; second, that whatever you wanted to do, it may or may not already have been done; and third, that writing example text is actually kind of difficult.',
+        commentCount: 5,
+      },
+      {
+        id: '6',
+        author: 'Author2ID',
+        content: 'So what I think is going on here is actually nothing in particular. This post, by the way, has an ID of 6, but it\'s actually the seventh post on the page, because that\'s how indexing works.',
+        commentCount: 0,
+      },
+      {
+        id: '7',
+        author: 'Author1ID',
+        content: 'In the end, it didn\'t work, but we discovered several things along the way: first, that whatever you do, you can\'t do anything you thought you maybe couldn\'t do unless you at least tell yourself you can try; second, that whatever you wanted to do, it may or may not already have been done; and third, that writing example text is actually kind of difficult.',
+        commentCount: 1012,
+      },
     ],
   };
 
@@ -21,9 +64,21 @@ export default class Stream extends React.Component {
     const { posts } = this.state;
     return (
       <div className="stream">
-        {posts.map(({ id, author, content }) => (
-          <Post key={id} author={author} content={content} />
-        ))}
+        {posts.map(
+          ({
+            id,
+            author,
+            content,
+            commentCount,
+          }) => (
+            <Post
+              key={id}
+              author={author}
+              content={content}
+              commentCount={commentCount}
+            />
+          ),
+        )}
       </div>
     );
   }
