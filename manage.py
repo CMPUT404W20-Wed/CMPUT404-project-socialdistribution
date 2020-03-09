3783cc845f3f373f6e9bba11b0427619696c32a5
abxjdorn <abxjdorn@users.noreply.github.com>
2020-03-09 12:23:47 -0600

    Switch settings module

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-            'social_backend.settings.development')
+            'social_backend.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
