c89560ed4d02b379a4500c881f0af54b215607d6
abxjdorn <abxjdorn@users.noreply.github.com>
2020-04-06 15:31:06 -0600

    ensure csrf cookie

--- social_backend/views.py
+++ social_backend/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def app(req):
     template = loader.get_template('social_backend/index.html')
     return HttpResponse(template.render({}, req))
